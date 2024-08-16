40a7d6666604c469bb46d606e1fe1c02ae1b46ff
luoyu-intel <yu.luo@intel.com>
2024-08-16 14:18:27 +0800

    1st

---
+++ oneapi_matmul/oneapi_matmul.cpp
@@ -0,0 +1,94 @@
+﻿// oneapi_matmul.cpp : Defines the entry point for the application.
+//
+
+#include "oneapi_matmul.h"
+#include <oneapi/mkl.hpp>
+#include <sycl/sycl.hpp>
+
+using namespace std;
+using namespace sycl;
+
+template<typename T>
+void mklgemm(queue& q, const std::vector<T>& ah, const std::vector<T>& bh,
+	std::vector<T>& ch, int m, int n, int k) {
+
+	// Create buffers that hold the data shared between the host and the devices.
+	// The buffer destructor is responsible to copy the data back to host when it
+	// goes out of scope.
+	auto a_buf = malloc_device<T>(m * k, q);
+	auto b_buf = malloc_device<T>(n * k, q);
+	auto c_buf = malloc_device<T>(m * n, q);
+
+	q.memcpy(a_buf, ah.data(), m * k * sizeof(T)).wait();
+	q.memcpy(b_buf, bh.data(), n * k * sizeof(T)).wait();
+	q.memset(c_buf, 0, m * n * sizeof(T)).wait();
+
+	timer<milliseconds> tm;
+	tm.start();
+	using trans = oneapi::mkl::transpose;
+	oneapi::mkl::blas::row_major::gemm(q, trans::N, trans::N, m, n, k, 1.f, a_buf, k, b_buf, n, 0.f
+		, c_buf, n, oneapi::mkl::blas::compute_mode::unset);
+	// Wait until compute tasks on GPU done
+	q.wait();
+	auto tms = tm.stop();
+	int iters = 1000.f / tms;
+	iters = std::max(1, iters);
+	iters = std::min(1000, iters);
+	q.memcpy(ch.data(), c_buf, m * n * sizeof(T)).wait();
+
+	tm.start();
+	for (size_t i = 0; i < iters; i++)
+	{
+		oneapi::mkl::blas::row_major::gemm(q, trans::N, trans::N, m, n, k, 1.f, a_buf, k, b_buf, n, 0.f
+			, c_buf, n, oneapi::mkl::blas::compute_mode::prefer_alternate);
+	}
+	q.wait();
+	auto avgt = tm.stop() / iters;
+	double FOPS = double(m) * k * n * 2;
+	double flops = FOPS / avgt / 1e3;
+	printf("%s %d (%d,%d,%d) time:%.2f ms FLOPs:%f\n", __FUNCTION__, __LINE__, m, n, k, avgt, flops);
+	sycl::free(a_buf, q);
+	sycl::free(b_buf, q);
+	sycl::free(c_buf, q);
+}
+
+void sgemmtest(queue& q, int m, int n, int k) {
+	std::vector<float> A(m * k), B(n * k), C(m * n), CCpu(m * n);
+	mklgemm<float>(q, A, B, C, m, n, k);
+}
+
+
+void hgemmtest(queue& q, int m, int n, int k) {
+	std::vector<half> A(m * k), B(n * k), C(m * n), CRef(m * n);
+	mklgemm<half>(q, A, B, C, m, n, k);
+}
+
+int main1(int argc, char* argv[]) {
+	// The default device selector will select the most performant device.
+	auto selector = default_selector_v;
+	// Create an exception handler for asynchronous SYCL exceptions
+	static auto exception_handler = [](sycl::exception_list e_list) {
+		for (std::exception_ptr const& e : e_list) {
+			try {
+				std::rethrow_exception(e);
+			}
+			catch (std::exception const& e) {
+#if _DEBUG
+				std::cout << "Failure" << std::endl;
+#endif
+				std::terminate();
+			}
+		}
+		};
+
+	queue q(selector, exception_handler);
+
+	// Print out the device information used for the kernel code.
+	std::cout << "Running on device: "
+		<< q.get_device().get_info<info::device::name>() << "\n";
+
+	sgemmtest(q, 4096, 4096, 4096);
+	hgemmtest(q, 4096, 4096, 4096);
+	std::cout << "Vector add successfully completed on device.\n";
+	return 0;
+}
---
+++ oneapi_matmul/oneapi_matmul.h
@@ -0,0 +1,119 @@
+﻿// oneapi_matmul.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <chrono>
+// TODO: Reference additional headers your program requires here.
+
+using milliseconds = std::chrono::milliseconds;
+using nanoseconds = std::chrono::nanoseconds;
+using microseconds = std::chrono::microseconds;
+template <typename _DUR = std::chrono::milliseconds>
+class timer {
+ public:
+  using sclock_t = std::chrono::steady_clock;
+  using stime_point_t = std::chrono::time_point<sclock_t>;
+
+  timer() { clear(); }
+
+  void start() { startT = sclock_t::now(); }
+
+  void clear() { startT = stime_point_t::min(); }
+
+  bool null_state() { return startT == stime_point_t::min(); }
+
+  float stop() { return static_cast<float>(std::chrono::duration_cast<_DUR>(sclock_t::now() - startT).count()); }
+
+  stime_point_t startT;
+};
+
+template <typename T>
+class minmax_statistics {
+ public:
+  minmax_statistics() { clear(); }
+
+  void clear() {
+    min_val = std::numeric_limits<T>::max();
+    max_val = std::numeric_limits<T>::min();
+    avg_val = 0;
+    count = 0;
+  }
+
+  void add(T _val) {
+    min_val = min_val > _val ? _val : min_val;
+    max_val = max_val < _val ? _val : max_val;
+    count += 1;
+    avg_val = (avg_val * (count - 1) + _val) / count;
+  }
+
+  T min_val, max_val, avg_val;
+  size_t count;
+};
+
+template <int _PRINT_CYCLE_MS = 100, typename _PRECISION = microseconds, typename _LOG_PRECISION = milliseconds>
+class timer_statistics_logger {
+ public:
+  typedef timer<milliseconds> log_timer_t;
+  timer_statistics_logger() {
+    clear();
+    log_ratio = static_cast<float>(std::chrono::duration_cast<_PRECISION>(_LOG_PRECISION(1)).count());
+  }
+
+  void clear() {
+    statis.clear();
+    logtm.clear();
+  }
+
+  void start() {
+    if (logtm.null_state()) {
+      logtm.start();
+    }
+    tm.start();
+  }
+
+  bool stop() {
+    auto elapsed = tm.stop();
+    statis.add(elapsed);
+    if (logtm.stop() >= _PRINT_CYCLE_MS) {
+      record();
+      clear();
+      logtm.start();
+      return true;
+    }
+    return false;
+  }
+
+  bool add(float time) {
+    statis.add(time);
+    if (logtm.stop() >= _PRINT_CYCLE_MS) {
+      record();
+      clear();
+      logtm.start();
+      return true;
+    }
+    return false;
+  }
+
+  const char* get_log_str() {
+    sprintf(str, "Min:%.4f, Max:%.4f, Average:%.4f", min_val, max_val, avg_val);
+    return str;
+  }
+  float min_val, max_val, avg_val;
+
+  void record() {
+    if (statis.count) {
+      min_val = statis.min_val / log_ratio;
+      max_val = statis.max_val / log_ratio;
+      avg_val = statis.avg_val / log_ratio;
+    }
+  }
+
+ private:
+  float log_ratio;
+  char str[256];
+  timer<_PRECISION> tm;
+  minmax_statistics<float> statis;
+  timer<milliseconds> logtm;
+};
